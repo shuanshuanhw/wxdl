8cce42dc943c24bc5925df04a49dc0c98e7453e0
shuanshuan <178650140@qq.com>
2022-04-26 08:57:27 +0800

    11

--- src/main/java/com/java1234/entity/HttpParame.java
+++ src/main/java/com/java1234/entity/HttpParame.java
@@ -21,7 +21,7 @@ public class HttpParame {
     public static final String REDIRECT_URI = "redirect_uri";
 
     // 网页授权回调地址
-    public static final String AUTHORIZATION_URL = "https://open.weixin.qq.com/connect/qrconnect?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+    public static final String AUTHORIZATION_URL = "https://open.weixin.qq.com/connect/qrconnect?appid=wxbdc5610cc59c1631&redirect_uri=https%3A%2F%2Fpassport.yhd.com%2Fwechat%2Fcallback.do&response_type=code&scope=snsapi_login&state=3d6be0a4035d839573b04816624a415e#wechat_redirect";
 
     // 通过code获取access_token
     public static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
